44f1d4b7619af0adf47e05bb58f5d05f0d1b1eef
Þorgils Árni Hjálmarsson <creep1g@gmail.com>
2019-04-03 21:33:43 +0000

    YAML implemented, refactored, missing some methods

--- lib/game.rb
+++ lib/game.rb
@@ -2,15 +2,17 @@ class Game
   def initialize()
     @board = []
     @missed = Array.new
+    @word = Array.new
   end
 
   def create_board(length)
     length.times {@board << "_"}
   end
 
-  def check_guess(guess, word)
-    if word.include?(guess)
-      update_board(guess, word)
+  def check_guess(guess)
+    if @word.include?(guess)
+      update_board(guess, @word)
+    elsif guess == 'save'
     elsif @missed.include?(guess)
       #you have already guessed this!
       return false
@@ -19,6 +21,41 @@ class Game
     end
   end
 
+  def pick_word
+    dictionary = Array.new
+    File.open("5desk.txt", 'r') do |f|
+      f.each_line do |line|
+        dictionary << line.split.map(&:to_s)
+      end
+    end
+    @word = dictionary[rand(dictionary.length())].join().downcase()
+    @word = @word.scan /\w/
+    return @word
+  end
+
+  def word
+    return @word
+  end
+
+  def game_won?()
+
+    if @board.include?("_")
+      return false
+    else
+      puts @board.join("").capitalize()
+      puts "Congratulations You WON!"
+      return true
+    end
+  end
+
+  def save()
+    yaml = YAML::dump(self)
+    Dir.mkdir("save_games") unless Dir.exists? "save_games"
+    filename = "save_games/#{DateTime.now.to_s}.sav"
+    save_file = File.new(filename, 'w')
+    save_file.write(yaml)
+  end
+  
   def update_board(guess, word)
     word.each_with_index do |char, index|
       if char.downcase() == guess
--- lib/main.rb
+++ lib/main.rb
@@ -1,44 +1,107 @@
-require_relative 'bot.rb'
 require_relative 'game.rb'
+require 'date'
+require 'yaml'
 
-bot = Bot.new
-word = bot.pick_word
-puts word
-def game_won?(board)
-  board = board.split()
-  if board.include?("_")
-    return false
-  else
-    puts board.join("").capitalize()
-    puts "Congratulations You WON!"
-    return true
+# puts word
+def intro
+  puts                 "                  Welcome to HANGMAN!"
+  puts "The rules are simple, the computer will choose a word"
+  puts     "and all you have to do is guess the right letters"
+  puts   "Each missed letter counts as a part of the mans body"
+  puts        "so choose your letters wisely or you will lose!"
+  start
+end
+
+def load
+  game_files = Dir.entries("save_games").select { |f| f.include?(".sav") }
+  game_files.each_with_index do |filename, index|
+    puts "[#{index}] #{filename}"
+  end
+
+  print ": "
+  index = gets.chomp
+  game_file = "save_games/#{game_files[index.to_i]}"
+  yaml = File.open(game_file).read
+  YAML::load(yaml)
+end
+
+def start
+  puts "Do you want to start a new game or load a saved game?[N/L]"
+  loop do
+    choice = gets.chomp.downcase()
+    if choice == 'l'
+      load_game
+     break
+    elsif choice == 'n'
+      start_new_game
+      break
+    else
+      puts "Type 'L' to load a save or type 'N' to start a new game"
+    end
   end
 end
 
-def game(word)
-  game = Game.new
-  game.create_board(word.length())
-  i = 0
-  while i != 8
-    puts game.board_to_s
-    puts game.missed_letters
-    guess = gets.chomp.downcase()
-    loop do
-      if guess.length() > 1 || guess.length() < 1
-        puts "You can only guess one letter!"
-        puts "Try again:"
-        guess = gets.chomp.downcase()
-      else
-        break
-      end
-    end
-    
-    game.check_guess(guess, word)
-    if game_won?(game.board_to_s) == true
+def load_game
+  $game = load
+  puts $game.word
+  # $word = game.word
+  game()
+end
+
+def start_new_game
+  $game = Game.new
+  $word = $game.pick_word
+  $game.create_board($word.length())
+  game()
+end
+
+def guess
+  print "Guess: "
+  guess = gets.chomp.downcase()
+  loop do
+    if guess == 'save'
+      $game.save()
+      break
+    elsif guess.length() > 1 || guess.length() < 1
+      puts "You can only guess one letter!"
+      puts "Try again:"
+      guess = gets.chomp.downcase()
+    else
       break
     end
-    i = game.missed_letters.split(", ").length()
+  end
+  return true if guess == 'save'
+  $game.check_guess(guess)
+  return false
+end
+
+def restart
+  puts "Would you like to play a new game or load from a save?[N/L]"
+  loop do
+    choice = gets.chomp.downcase()
+    if choice == 'n'
+      start_new_game
+      break
+    elsif choice == 'l'
+      load_game
+      break
+    end
+  end
+
+end
+
+def game()
+  puts $word
+  i = 0
+  while i != 8
+    puts $game.board_to_s
+    puts $game.missed_letters unless $game.missed_letters.length() == 16
+    break if guess == true
+    restart() if $game.game_won? == true
+    i = $game.missed_letters.split(", ").length()
   end
   puts "You hung that poor man!" if i == 8
+  #TODO create loose method
 end
-game(word)
+
+intro()
